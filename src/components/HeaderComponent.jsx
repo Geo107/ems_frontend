c0afdfe52ab669c190a303e83bb6bb917a1b90a1
Geo107 <geombenny@gmail.com>
2025-07-27 22:21:44 +0530

    Safepoint

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -5,6 +5,7 @@ const  HeaderComponent = () => {
     <header>
         <nav className='navbar navbar-dark bg-dark'>
             <a className="navbar-brand" href="https://www.google.com"> EMS </a>
+            <a href=""></a>
         </nav>
     </header>
   )
